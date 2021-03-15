3fc70a1a9d77565f3421b7ca0d343354b7e9b4e9
Maria-7514 <evangeline73.mjhe@gmail.com>
2021-03-15 18:43:06 +0000

    traffic light listo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,62 @@
-import React from "react";
-
+import React, { useState } from "react";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	let [luz1, setluz1] = useState("Apagada");
+	let [luz2, setluz2] = useState("Apagada");
+	let [luz3, setluz3] = useState("Apagada");
+
+	function cambio1() {
+		if (luz1 === "Apagada") {
+			setluz1("Encendida");
+		} else {
+			setluz1("Apagada");
+		}
+	}
+
+	function cambio2() {
+		if (luz2 === "Apagada") {
+			setluz2("Encendida");
+		} else {
+			setluz2("Apagada");
+		}
+	}
+
+	function cambio3() {
+		if (luz3 === "Apagada") {
+			setluz3("Encendida");
+		} else {
+			setluz3("Apagada");
+		}
+	}
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="top"></div>
+			<div className=" luces">
+				<button
+					onClick={cambio1}
+					className={
+						"luz roja" + (luz1 == "Apagada" ? " shadow" : "")
+					}>
+					{setluz1}
+				</button>
+				<button
+					onClick={cambio2}
+					className={
+						" luz amarilla" + (luz2 == "Apagada" ? " shadow" : "")
+					}>
+					{setluz2}
+				</button>
+				<button
+					onClick={cambio3}
+					className={
+						" luz verde" + (luz3 == "Apagada" ? " shadow" : "")
+					}>
+					{setluz3}
+				</button>
+			</div>
 		</div>
 	);
 }
